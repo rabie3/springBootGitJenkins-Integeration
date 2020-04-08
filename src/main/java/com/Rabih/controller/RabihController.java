98785d92afe6826efa6e4ebe773e086a435ef935
rabie3 <rabih.talaba@gmail.com>
2020-04-08 12:05:10 -0400

    change3

--- src/main/java/com/Rabih/controller/RabihController.java
+++ src/main/java/com/Rabih/controller/RabihController.java
@@ -9,6 +9,6 @@ public class RabihController {
 	@GetMapping("/")
 public String getMessage()
 {
-	return "Welcome to git and jenkins inteagration with update";
+	return "Welcome to git and jenkins inteagration with updates";
 }
 }
